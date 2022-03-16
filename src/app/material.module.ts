ecf9defbe39796ecef2e20584d6ec67bab0bc4a0
kiyimba1 <edwineddie54@gmail.com>
2022-03-16 11:53:40 +0300

    Fist steps to LOB App

---
+++ src/app/material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { MatToolbarModule } from '@angular/material/toolbar'
+
+
+
+const modules = [MatButtonModule, MatIconModule, MatToolbarModule]
+
+
+
+@NgModule({
+  declarations: [],
+  imports: modules,
+  exports: modules
+})
+export class MaterialModule { }
